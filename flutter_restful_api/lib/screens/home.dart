8e7d3f64abad4499fcc84f4e55349cec2a891377
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2023-01-23 16:51:59 +0800

    Update home.dart
    make home page

--- flutter_restful_api/lib/screens/home.dart
+++ flutter_restful_api/lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_restful_api/screens/all_category.dart';
+import 'package:flutter_restful_api/screens/cart.dart';
+import 'package:flutter_restful_api/screens/product_detail.dart';
 import 'package:flutter_restful_api/services/api_service.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,6 +15,26 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.redAccent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AllCategory()),
+              );
+            },
+            icon: const Icon(Icons.view_list),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Cart()),
+              );
+            },
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: ApiServices().getAllPosts(),
@@ -30,6 +53,16 @@ class HomePage extends StatelessWidget {
                     ),
                     subtitle:
                         Text("Price - \$ ${snapshot.data[index]['price']}"),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProductDetail(
+                            snapshot.data[index]['id'],
+                          ),
+                        ),
+                      );
+                    },
                   );
                 },
               ),
